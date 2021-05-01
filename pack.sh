5ef90cb4083db1abe4e6ac8c7b133834fc76b5f7
Ben Bemis <bbemis@alumni.purdue.edu>
2021-05-01 16:22:34 -0500

    gitignore

--- pack.sh
+++ pack.sh
@@ -1,3 +1,6 @@
-GOOS=linux GOARCH=amd64 go build -o main main.go
 
-zip main.zip main
+OUTPUT_DIR="bin"
+
+GOOS=linux GOARCH=amd64 go build -o "${OUTPUT_DIR}/main" main.go
+
+zip "${OUTPUT_DIR}/main" "${OUTPUT_DIR}/main"
